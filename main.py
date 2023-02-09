960b551ab1bbcfc4a7b3620e5e69ed3d2c0c9368
Adigeefe <39971647+adigeefe@users.noreply.github.com>
2023-02-10 02:18:55 +0300

    ADD: Keywords

--- main.py
+++ main.py
@@ -12,33 +12,35 @@ class MyClient(discord.Client):
         if message.author == client.user:
             return
 
-        if message.content.startswith('/start_scanner') or message.content.startswith('/startscanner'):       
-            text_channel_list = []
+        if message.content.startswith('/start_scanner') or message.content.startswith('/startscanner'):
+            if message.author.id == 448224340530561043 or message.author.id == 637386194283659314 or message.author.id == 839183314833244210:
+                text_channel_list = []
 
-            currentDateAndTime = datetime.now().strftime("%Y_%m_%d-%H_%M" )
-            try:
-                os.system(f"mkdir -p logs/{currentDateAndTime}");print(f'\033[93m logs/{currentDateAndTime} has been created.\033[0m')
-            except Exception as e:
-                print("Error:", e)
+                currentDateAndTime = datetime.now().strftime("%Y_%m_%d-%H_%M" )
+                try:
+                    print(message.author)
+                    os.system(f"mkdir -p logs/{currentDateAndTime}");print(f'\033[93m logs/{currentDateAndTime} has been created.\033[0m')
+                except Exception as e:
+                    print("Error:", e)
 
-            for server in client.guilds:
-                for channel in server.channels:
-                    if str(channel.type) == 'text':
-                        text_channel_list.append(channel)
-            for channel_name in text_channel_list:
+                for server in client.guilds:
+                    for channel in server.channels:
+                        if str(channel.type) == 'text':
+                            text_channel_list.append(channel)
+                for channel_name in text_channel_list:
 
-                async for message in channel_name.history(limit=200, oldest_first=True):
+                    async for message in channel_name.history(limit=200, oldest_first=True):
 
-                    with open('keywords.data', "r") as x:
-                        keyword_data=x.read()
-                        keywordlist = list(keyword_data.split("\n"))
-                    for keyword in keywordlist:
-                        if keyword in message.content:
-                            #print(astimezone(timezone("Asia/Istanbul")).strftime( "%Y-%m-%d %X"))
+                        with open('keywords.data', "r") as x:
+                            keyword_data=x.read()
+                            keywordlist = list(keyword_data.split("\n"))
+                        for keyword in keywordlist:
+                            if keyword in message.content:
+                                #print(astimezone(timezone("Asia/Istanbul")).strftime( "%Y-%m-%d %X"))
 
-                            
-                            with codecs.open(f'logs/{currentDateAndTime}/{channel_name}.log', 'w', 'utf-8') as f:
-                                f.write(f'{message.created_at.astimezone(timezone("Asia/Istanbul")).strftime( "%Y-%m-%d %X")} | #{message.channel} | {message.author} => {message.content}\n')
+                    
+                                with codecs.open(f'logs/{currentDateAndTime}/{channel_name}.log', 'w', 'utf-8') as f:
+                                    f.write(f'{message.created_at.astimezone(timezone("Asia/Istanbul")).strftime( "%Y-%m-%d %X")} | #{message.channel} | {message.author} => {message.content}\n')
 
         if message.content.startswith(''):
             print(f'{message.created_at.astimezone(timezone("Asia/Istanbul")).strftime( "%Y-%m-%d %X")} | #{message.channel} | {message.author} => {message.content}')
